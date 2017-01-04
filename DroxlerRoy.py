1563a253129c488f09a883a69ca2ff434a010e39
axelroy <royaxe@gmail.com>
2017-01-04 13:38:54 +0100

    Ajout du rendu du meilleur tracé au l'écran. Il faudra séparer les tâches en MVC. Reste la mutation à faire

--- DroxlerRoy.py
+++ DroxlerRoy.py
@@ -10,6 +10,9 @@ import random
 # Contient le tableau de villes. Une fois instancié, il n'est plus modifié.
 cities = None
 population_size = 10
+WHITE = (255,255,255)
+BLACK = (0,0,0)
+POINTSIZE = 3
 
 ################################################################################
 #  Algorithme génétique
@@ -53,13 +56,54 @@ def selection(population):
     return population
 
 def crossing(population, size):
+    """ Principe global de mutation : Mutation XO.
+        On selectionne deux Chromosomes x et y parmis la population.
+        On détermine une section où on va insérer la section de y dans le même endroit de x.
+        Il faut pour ceci préparer x à recevoir les gènes de y en :
+            Déterminant les valeurs de la portion de y qui sera insérer.
+            Remplacer ces valeurs dans x par un marqueur.
+            Mettre en place ces marqueurs à la position de la section que l'on échange.
+            Pour ceci, on condense tous les indexes sans les marqueurs, que l'on décale
+            par n rotations à droite, où n est le nombre de marqueurs entre la fin de la section
+            et la fin des gênes.
+            A la fin, on insère la section de y.
+
+            exemple complet :
+
+            Chromosomes retenus
+            [8, 7, 2, 3, 0, 5, 1, 6, 4, 9]] : Cost : 2433.6255091876656
+            [4, 9, 0, 3, 5, 6, 2, 7, 1, 8]] : Cost : 2468.848455299176
+
+            Section (valeurs) à échanger (choisie arbitrairement, indexs 3 à 5)
+            [3, 5, 6]
+
+            X sans les valeurs de la section
+            [8, 7, 2, None, 0, None, 1, None, 4, 9]
+
+            Nombre de None après l'index 5
+            1
+
+            Liste sans les None, avant décalage
+            [8, 7, 2, 0, 1, 4, 9]
+            Liste sans les None, après décalage
+            [7, 2, 0, 1, 4, 9, 8]
+
+            Portion à insérer
+            [3, 5, 6]
+
+            Nouveaux gênes après croisements
+            [7, 2, 0, 3, 5, 6, 1, 4, 9, 8]
+
+    """
+    # TODO : Range dynamique
     # start_xo_index = int(len(population[0].genes) / 2 - len(population[0].genes) / 4)
     start_xo_index = 3
     end_xo_index = 5
     nb_to_create = size - len(population)
 
     for chromosome_index in range(0, nb_to_create):
-        # print(chromosome_index, " len", len(population))
+        # Choix des chromosomes, pour le moment consécutifs
+        # TODO : Changer le choix des échantillons dans la population
         if chromosome_index < len(population):
             chromosome_x = population[chromosome_index - len(population)]
             chromosome_y = population[chromosome_index - len(population) + 1]
@@ -67,26 +111,28 @@ def crossing(population, size):
             chromosome_x = population[chromosome_index - len(population)]
             chromosome_y = population[0]
 
-        print("Chromosomes retenus")
-        print(chromosome_x)
-        print(chromosome_y)
+        # Détermination des valeurs à supprimer dans x, tirées de la portion y
         list_to_replace = chromosome_y.genes[start_xo_index:end_xo_index+1]
+
+        # Remplacement de ces valeurs dans x avec des None
         new_genes_list = [value if value not in list_to_replace else None for value in chromosome_x.genes]
-        print(list_to_replace)
-        print(new_genes_list)
+
+        # Comptage du nombre de None à droite de la section (pour le décalage)
         nb_none_right = new_genes_list[end_xo_index+1:].count(None)
-        print(nb_none_right)
+
+        # Suppression des None dans la liste pour les rotations
         new_genes_list = [value for value in new_genes_list if not value == None]
+
+        # Rotation à droite des éléments
         for counter in range(0,nb_none_right):
             new_genes_list.insert(len(new_genes_list), new_genes_list.pop(0))
-        print(new_genes_list)
         list_to_insert = chromosome_y.genes[start_xo_index:end_xo_index+1]
-        print(list_to_insert)
+
+        # Insertion des valeurs de y dans la section préparée
         new_genes_list[start_xo_index:start_xo_index] = list_to_insert
-        print(new_genes_list)
-        print("===================")
-        # population.append(crossing_chromosome)
-        population.append(population[0])
+
+        # Ajout du nouveau chromosome à la population
+        population.append(Chromosome(new_genes_list))
 
     return population
 
@@ -108,6 +154,8 @@ def solve(cities_list, rounds = 100, window = None):
     print(rounds)
 
     while rounds > 0:
+        if window != None:
+            draw_best_path(population, window)
         print("Selection")
         population = selection(population)
         print("Croisement")
@@ -176,10 +224,26 @@ def main(argv):
 #  Affichage
 ################################################################################
 
+def clear_window(window):
+    window.fill(BLACK)
+    pygame.draw.rect(window, WHITE, [0, 0, 50, 20], 2)
+
+    for point in cities:
+        pygame.draw.rect(window, WHITE, [point.pos.x, point.pos.y, POINTSIZE, POINTSIZE])
+
+def draw_best_path(population, window):
+    clear_window(window)
+
+    list_points = []
+    best_genes_list = population[0].genes
+    for gene in best_genes_list:
+        list_points.append(cities[gene].pos)
+    pygame.draw.lines(window, WHITE, False, list_points, 1)
+
+
+
 def display(cities_list = None):
     LEFTCLICK = 1                     # Défini ainsi dans pygame
-    WHITE = (255,255,255)
-    POINTSIZE = 5
 
     window = pygame.display.set_mode((500, 500))
 
@@ -207,7 +271,7 @@ def display(cities_list = None):
             # Gestion des événements souris
             if event.type == MOUSEBUTTONDOWN and event.button == LEFTCLICK:
                 if over_launch:
-                    solve(cities_list, 2, window)
+                    solve(cities_list, 10, window)
                 else:
                     x_mouse, y_mouse = event.pos[0], event.pos[1]
                     # Attention : envoie une liste de tuples! La synthaxe est fine.
